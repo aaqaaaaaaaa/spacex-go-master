894f783d4ae5eb2bdd446c503bdba466ab4cc656
aaqaaaaaaaa <jalolxonkamolxonov@gmail.com>
2022-09-09 11:11:16 +0500

    Initial commit

---
+++ lib/utils/translate.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_i18n/flutter_i18n.dart';
+
+extension Translate on BuildContext {
+  String translate(
+     String key, {
+     Map<String, String> parameters,
+  }) {
+    try {
+      return FlutterI18n.translate(
+        this,
+        key,
+        translationParams: parameters,
+      );
+    } catch (_) {
+      return key;
+    }
+  }
+}
